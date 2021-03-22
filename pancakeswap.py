a5d2e841ced7ca17591c36b28d2affe5c3d6816a
Josue Guevara <josueguevara291@gmail.com>
2021-03-22 18:29:59 -0500

    Segunda versión de script: compra realizada

--- pancakeswap.py
+++ pancakeswap.py
@@ -6,26 +6,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-import socket
 
 def element_presence(by, xpath, time):
     element_present = EC.presence_of_element_located((By.XPATH, xpath))
     WebDriverWait(driver, time).until(element_present)
 
-def is_connected():
-    try:
-        # conectarse al host: nos dice si el host es en realidad
-        # accesible
-        socket.create_connection(("www.google.com", 80))
-        return True
-    except:
-        is_connected()
-
 chrome_options = ChromeOptions()
 chrome_options.add_extension('D:\Wstp\metamask.crx')
 
 driver = webdriver.Chrome(executable_path="D:\Wstp\chromedriver.exe", options=chrome_options)
-driver.get("https://exchange.pancakeswap.finance/#/swap")
+driver.get("https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x190b589cf9Fb8DDEabBFeae36a813FFb2A702454")
+driver.switch_to_window(driver.window_handles[-1])
+sleep(1)
+driver.find_element_by_xpath('//*[@id="understand-checkbox"]').click()
+sleep(1)
+driver.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div/div/div[7]/button').click()
 #sleep(2)
 
 semilla = open('D:\Wstp\semilla.txt').readline().strip()
@@ -34,7 +29,7 @@ nombre_red = "Binance Smart Chain Mainnet"
 url = "https://bsc-dataseed1.ninicoin.io"
 id_cadena = "56"
 
-driver.switch_to_window(driver.window_handles[-1])
+#driver.switch_to_window(driver.window_handles[-1])
 sleep(1)
 driver.switch_to_window(driver.window_handles[0])
 element_presence(
@@ -117,9 +112,45 @@ sleep(1)
 driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
 sleep(1)
 
+
+
+#driver.execute_script("window.open('');")
 driver.switch_to_window(driver.window_handles[-1])
+sleep(1)
+#driver.get("https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x190b589cf9Fb8DDEabBFeae36a813FFb2A702454")
+#sleep(1)
+#driver.find_element_by_xpath('//*[@id="understand-checkbox"]').click()
+#sleep(1)
+#driver.find_element_by_xpath('/html/body/reach-portal/div[3]/div/div/div/div/div/div[7]/button').click()
 
+element_presence(
+        By.XPATH, '//*[@id="swap-currency-input"]/div/div[2]/input', 10)
+input_moneda = driver.find_element_by_xpath('//*[@id="swap-currency-input"]/div/div[2]/input')
 
+cantidad = 1
+input_moneda.send_keys(cantidad)
+sleep(1)
+
+element_presence(
+        By.XPATH, '//*[@id="swap-button"]', 60)
+swap = driver.find_element_by_xpath('//*[@id="swap-button"]')
+swap.click()
+driver.find_element_by_xpath('//*[@id="confirm-swap-or-send"]').click()
+
+driver.switch_to_window(driver.window_handles[0])
+sleep(1)
+driver.refresh()
+
+element_presence(
+        By.XPATH, '//*[@id="app-content"]/div/div[4]/div/div[3]/div[3]/footer/button[2]', 60)
+
+driver.find_element_by_xpath('//*[@id="app-content"]/div/div[4]/div/div[3]/div[3]/footer/button[2]').click()
+
+sleep(1)
+driver.switch_to_window(driver.window_handles[-1])
+element_presence(
+        By.XPATH, '/html/body/reach-portal[4]/div[3]/div/div/div/div/div/div[3]/button', 60)
+driver.find_element_by_xpath('/html/body/reach-portal[4]/div[3]/div/div/div/div/div/div[3]/button').click()
 
 
 #driver.execute_script("window.open('');")
